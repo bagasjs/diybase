09c9c63211e0d40a971342ca2bf8f174722dee87
bagasjs <nakanoyoichi83@gmail.com>
2024-03-19 05:35:03 +0700

    Completing initialization command

--- cmd/init.go
+++ cmd/init.go
@@ -2,12 +2,26 @@ package cmd
 
 import (
 	"log"
+	"os"
+	"path"
 
 	"github.com/spf13/cobra"
 )
 
+const RESOURCE_DIR="res"
+const DIRECTORY_PERMISSIONS=0755
+
 func onInit(cmd *cobra.Command, args []string) {
-    log.Print("Initializing environment")
+    log.Println("Initializing environment")
+    if cwd, err := os.Getwd(); err == nil {
+        os.MkdirAll(path.Join(cwd, RESOURCE_DIR), DIRECTORY_PERMISSIONS)
+        os.MkdirAll(path.Join(cwd, RESOURCE_DIR, "views"), DIRECTORY_PERMISSIONS)
+        os.MkdirAll(path.Join(cwd, RESOURCE_DIR, "static"), DIRECTORY_PERMISSIONS)
+        log.Println("Environment is initialized")
+        log.Println("Execute the migration command next!")
+    } else {
+        log.Fatal(err)
+    }
 }
 
 var initCmd = &cobra.Command {
